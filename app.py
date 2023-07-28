298a0c4770e1b0493a3a41efc80366fc59618a29
RStar2022 <risingstar2880@gmail.com>
2023-07-29 02:38:19 +0900

    add cors

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, current_app, request, Response
 from flask_restful import Api
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from resources.profile import ProfileApi, ProfilesApi
 from resources.user import SignupApi, LoginApi
+from flask_cors import CORS
 from database.db import db
 
 app = Flask(__name__)
+cors = CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://hacks:hacks123@localhost/fittech"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config.from_envvar('ENV_FILE_LOCATION')
@@ -27,6 +29,12 @@ api.add_resource(ProfilesApi, '/profiles/<id>')
 
 bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
+
+with app.app_context():
+    @current_app.before_request
+    def basic_authentication():
+        if request.method.lower() == 'options':
+            return Response()
 
 if __name__ ==  '__main__':
     app.run(debug=False, host='0.0.0.0', port=80)
